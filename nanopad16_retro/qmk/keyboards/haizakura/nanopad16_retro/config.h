14d2698e23c9951a77be7e5c720a76f604313f7d
HAIZAKURA <hhbilly99@gmail.com>
2022-03-01 20:21:18 +0800

    Update config.h

--- nanopad16_retro/qmk/keyboards/haizakura/nanopad16_retro/config.h
+++ nanopad16_retro/qmk/keyboards/haizakura/nanopad16_retro/config.h
@@ -38,9 +38,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
-// Connects each switch in the dip switch to the GPIO pin of the MCU
-#define DIP_SWITCH_PINS { C5, B1 }
-
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
 
