4aef76330d1a200c7761910ecd835deac674c786
HAIZAKURA <hhbilly99@gmail.com>
2022-03-02 09:54:40 +0800

    Update config.h

--- nanopad16_retro/qmk/keyboards/haizakura/nanopad16_retro/config.h
+++ nanopad16_retro/qmk/keyboards/haizakura/nanopad16_retro/config.h
@@ -53,3 +53,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   /*== all animations enable ==*/
   #define RGBLIGHT_ANIMATIONS
 #endif
+
+/* disable these deprecated features by default */
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
