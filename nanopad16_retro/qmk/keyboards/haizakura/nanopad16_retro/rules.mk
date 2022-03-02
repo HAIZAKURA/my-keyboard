81770f35f61871344cac4f706fa539fa5c53b126
HAIZAKURA <hhbilly99@gmail.com>
2022-03-02 13:42:10 +0800

    Update rules.mk

--- nanopad16_retro/qmk/keyboards/haizakura/nanopad16_retro/rules.mk
+++ nanopad16_retro/qmk/keyboards/haizakura/nanopad16_retro/rules.mk
@@ -1,9 +1,6 @@
 # MCU name
 MCU = atmega328p
 
-# Processor frequency
-F_CPU = 16000000
-
 # Bootloader selection
 BOOTLOADER = usbasploader
 
@@ -15,6 +12,6 @@ CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no	        # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 UNICODE_ENABLE = yes        # Unicode
