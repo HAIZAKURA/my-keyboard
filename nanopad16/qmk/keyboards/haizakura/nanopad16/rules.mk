92e3110763522d77aec01fb05ec037c1249bcae1
HAIZAKURA <hhbilly99@gmail.com>
2022-03-06 12:43:24 +0800

    Update rules.mk

--- nanopad16/qmk/keyboards/haizakura/nanopad16/rules.mk
+++ nanopad16/qmk/keyboards/haizakura/nanopad16/rules.mk
@@ -5,15 +5,14 @@ MCU = STM32F103
 BOOTLOADER = stm32duino
 
 # Build Options
-#   change yes to no to disable
-#
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no        # Commands for debug and configuration
+COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
-UNICODE_ENABLE = yes         # Unicode
-RGBLIGHT_ENABLE = yes        # Enable WS2812 RGB underlight.
+UNICODE_ENABLE = yes        # Unicode
+RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight
+ENCODER_ENABLE = yes        # Enable encoder
