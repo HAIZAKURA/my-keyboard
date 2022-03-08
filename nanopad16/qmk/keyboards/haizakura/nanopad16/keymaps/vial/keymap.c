18b3f9e11516140f31bb288a7d20b31a2d9e5779
HAIZAKURA <hhbilly99@gmail.com>
2022-03-08 16:49:29 +0800

    Update keymap.c

--- nanopad16/qmk/keyboards/haizakura/nanopad16/keymaps/vial/keymap.c
+++ nanopad16/qmk/keyboards/haizakura/nanopad16/keymaps/vial/keymap.c
@@ -11,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-------------------|
    * |   *|   /|   =|   .|
    * |-------------------|
-   * |   1|    |   2|   3|
+   * |   1|        2|   3|
    * '-------------------'
    */
   [0] = LAYOUT(
@@ -19,28 +19,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_P5,     KC_P6,    KC_P7,   KC_P8,
     KC_P9,     KC_P0,  KC_PLUS, KC_MINS,
     KC_ASTR, KC_SLSH, KC_ENTER,  KC_DOT,
-    KC_P1,     KC_NO,    KC_P2,   KC_P4
+    KC_P1,               KC_P2,   KC_P4
   ),
   [1] = LAYOUT(
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS,   KC_NO, KC_TRNS, KC_TRNS
+    KC_TRNS,          KC_TRNS, KC_TRNS
   ),
   [2] = LAYOUT(
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS,   KC_NO, KC_TRNS, KC_TRNS
+    KC_TRNS,          KC_TRNS, KC_TRNS
   ),
   [3] = LAYOUT(
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS,   KC_NO, KC_TRNS, KC_TRNS
+    KC_TRNS,          KC_TRNS, KC_TRNS
   )
 };
 
