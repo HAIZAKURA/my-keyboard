a4eea87db1eed613ace89060c70b5c465e242f30
HAIZAKURA <hhbilly99@gmail.com>
2022-03-21 10:29:28 +0800

    Create config.h

---
+++ nanopad9/qmk/keyboards/haizakura/nanopad9/config.h
@@ -0,0 +1,50 @@
+/*
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID    0xFEED
+#define PRODUCT_ID   0x0003
+#define DEVICE_VER   0x0001
+#define MANUFACTURER HAIZAKURA
+#define PRODUCT      NanoPad 9
+
+/* key matrix size */
+#define MATRIX_ROWS 3
+#define MATRIX_COLS 3
+
+/* nanopad16 PCB default pin-out */
+#define MATRIX_ROW_PINS { C7, B5, B6 }
+#define MATRIX_COL_PINS { C6, D3, D2 }
+#define UNUSED_PINS
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 5
+
+// The pin connected to the data pin of the LEDs
+#define RGB_DI_PIN B7
+#define RGBLED_NUM 9
+#define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
