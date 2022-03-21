598ed8f445c85d02bea068e3fa415753d78d879a
HAIZAKURA <hhbilly99@gmail.com>
2022-03-21 10:29:46 +0800

    Create keymap.c

---
+++ nanopad9/qmk/keyboards/haizakura/nanopad9/keymaps/vial/keymap.c
@@ -0,0 +1,33 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  /* Default Layer
+   * .--------------.
+   * |   1|   2|   3|
+   * |--------------|
+   * |   5|   6|   7|
+   * |--------------|
+   * |   9|   0|   +|
+   * '--------------'
+   */
+  [0] = LAYOUT(
+    KC_P1,     KC_P2,    KC_P3,
+    KC_P5,     KC_P6,    KC_P7,
+    KC_P9,     KC_P0,  KC_PLUS
+  ),
+  [1] = LAYOUT(
+    KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS
+  ),
+  [2] = LAYOUT(
+    KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS
+  ),
+  [3] = LAYOUT(
+    KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS
+  )
+};
