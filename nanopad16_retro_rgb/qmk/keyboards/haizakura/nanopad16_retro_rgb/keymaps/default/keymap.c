8ca4b0fb17281cc89e10ae2d8f724918ca26a486
HAIZAKURA <hhbilly99@gmail.com>
2022-03-28 09:41:29 +0800

    Update keymap.c

--- nanopad16_retro_rgb/qmk/keyboards/haizakura/nanopad16_retro_rgb/keymaps/default/keymap.c
+++ nanopad16_retro_rgb/qmk/keyboards/haizakura/nanopad16_retro_rgb/keymaps/default/keymap.c
@@ -11,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-------------------|
    * |   *|   /|   =|   .|
    * |-------------------|
-   * |   1|   2|   3|   4|
+   * |   1|    |    |   4|
    * '-------------------'
    */
   [0] = LAYOUT(
@@ -19,6 +19,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_P5,     KC_P6,    KC_P7,   KC_P8,
     KC_P9,     KC_P0,  KC_PLUS, KC_MINS,
     KC_ASTR, KC_SLSH, KC_ENTER,  KC_DOT,
-    KC_P1,     KC_P2,    KC_P3,   KC_P4
+    KC_P1,     KC_NO,    KC_NO,   KC_P4
   )
 };
